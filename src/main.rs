820010e3815f2766e9371f5f4bb4c24ffa7684ba
Matthew Garcia <gmatthew2765@gmail.com>
2026-01-02 19:24:49 -0800

    integrated clap

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,7 @@ struct Cli {
 }
 
 fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
-
-    let args = Cli {
-        pattern,
-        path: std::path::PathBuf::from(path),
-    };
+    let args = Cli::parse();
 
     println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
 }
