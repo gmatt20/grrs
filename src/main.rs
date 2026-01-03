37b74264877c7a04b36309baa5544096774ed87e
Matthew Garcia <gmatthew2765@gmail.com>
2026-01-03 13:06:58 -0800

    shorthanding

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,14 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.path).expect("could not read the file");
+    let content = std::fs::read_to_string(&args.path)?;
 
     for line in content.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
     }
+    Ok(())
 }
